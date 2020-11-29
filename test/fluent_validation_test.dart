ef6e0b2710a724aa0618b54ee61d554d8fe99212
evertonrps <evertonrps@outlook.com>
2020-11-29 12:43:59 -0300

    add optional custom messages

--- test/fluent_validation_test.dart
+++ test/fluent_validation_test.dart
@@ -5,9 +5,55 @@ import 'package:test/test.dart';
 import 'models/test_user.dart';
 
 void main() {
-  test('isNull validator returns errors correctly', () => TestUserNullValidator().runTest());
-  test('isNotNull validator returns errors correctly', () => TestNotNullValidator().runTest());
-  test('lessThan validator returns errors correctly', () => TestLessThanValidator().runTest());
+  test('isNull validator returns errors correctly',
+      () => TestUserNullValidator().runTest());
+  test('isNotNull validator returns errors correctly',
+      () => TestNotNullValidator().runTest());
+  test('lessThan validator returns errors correctly',
+      () => TestLessThanValidator().runTest());
+  test('withMessage validator returns errors correctly',
+      () => WithMessageValidator().runTest());
+  test('withoutMessage validator returns errors correctly',
+      () => WithoutMessageValidator().runTest());
+}
+
+class WithMessageValidator extends AbstractValidator<TestUser> {
+  WithMessageValidator() {
+    ruleFor((TestUser user) => user.age)
+        .isNull(message: "Custom message value must be null");
+  }
+
+  void runTest() {
+    final String expectedMessage = 'Custom message value must be null';
+    final TestUser testUser = TestUser(age: 18, name: 'Ryan');
+    final TestUser testUserTwo = TestUser(age: null, name: 'Ryan');
+
+    final ValidationResult validationResult = validate(testUser);
+    final ValidationResult validationResultTwo = validate(testUserTwo);
+
+    expect(validationResult.hasError, isTrue);
+    expect(validationResult.messages.first, expectedMessage);
+    expect(validationResultTwo.hasError, isFalse);
+  }
+}
+
+class WithoutMessageValidator extends AbstractValidator<TestUser> {
+  WithoutMessageValidator() {
+    ruleFor((TestUser user) => user.age).isNull();
+  }
+
+  void runTest() {
+    final String expectedMessage = 'Value must be null';
+    final TestUser testUser = TestUser(age: 18, name: 'Ryan');
+    final TestUser testUserTwo = TestUser(age: null, name: 'Ryan');
+
+    final ValidationResult validationResult = validate(testUser);
+    final ValidationResult validationResultTwo = validate(testUserTwo);
+
+    expect(validationResult.hasError, isTrue);
+    expect(validationResult.messages.first, expectedMessage);
+    expect(validationResultTwo.hasError, isFalse);
+  }
 }
 
 class TestLessThanValidator extends AbstractValidator<TestUser> {
@@ -18,7 +64,7 @@ class TestLessThanValidator extends AbstractValidator<TestUser> {
   void runTest() {
     final TestUser testUser = TestUser(age: 18, name: 'Ryan');
     final TestUser testUserTwo = TestUser(age: 24, name: 'Ryan');
-    
+
     final ValidationResult validationResult = validate(testUser);
     final ValidationResult validationResultTwo = validate(testUserTwo);
 
@@ -28,14 +74,14 @@ class TestLessThanValidator extends AbstractValidator<TestUser> {
 }
 
 class TestNotNullValidator extends AbstractValidator<TestUser> {
-  TestUserNullValidator() {
+  TestNotNullValidator() {
     ruleFor((TestUser user) => user.age).notNull();
   }
 
   void runTest() {
     final TestUser testUser = TestUser(age: null, name: 'Ryan');
     final TestUser testUserTwo = TestUser(age: 24, name: 'Ryan');
-    
+
     final ValidationResult validationResult = validate(testUser);
     final ValidationResult validationResultTwo = validate(testUserTwo);
 
@@ -52,7 +98,7 @@ class TestUserNullValidator extends AbstractValidator<TestUser> {
   void runTest() {
     final TestUser testUser = TestUser(age: null, name: 'Ryan');
     final TestUser testUserTwo = TestUser(age: 24, name: 'Ryan');
-    
+
     final ValidationResult validationResult = validate(testUser);
     final ValidationResult validationResultTwo = validate(testUserTwo);
 
