19bb4fab4c46313e3395eb8bd61b504d721351ce
Ryan Dixon <ryandixon1993@gmail.com>
2021-12-07 16:18:19 +0000

    Update example

--- example/main.dart
+++ example/main.dart
@@ -6,7 +6,15 @@ void main() {
   final User user = User("Ryan", "Sample Address", 26);
   final UserValidator userValidator = UserValidator();
   final ValidationResult validationResult = userValidator.validate(user);
+
   if (validationResult.hasError) {
-    // Handle Error
+    print('Got an error: $validationResult');
   }
+
+  final User userTwo = User("Ryan", "Sample Address", 11);
+  final UserValidator userTwoValidator = UserValidator();
+  final ValidationResult validationTwoResult = userTwoValidator.validate(userTwo);
+
+  print('First validator results: ${validationResult.errors.length}');
+  print('Second validator results: ${validationTwoResult.errors.length}');
 }
